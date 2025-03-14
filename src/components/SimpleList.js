ed5fffb66201131adcae72493894e9cc842604ec
bartoszK-zst <krol.bartosz@zst.radom.pl>
2025-03-14 10:34:49 +0100

    Można już swobonie dodawać i usuwać elementy z listy

--- src/components/SimpleList.js
+++ src/components/SimpleList.js
@@ -3,13 +3,49 @@ import { useState } from "react";
 export default function SimpleList(props){
     const [names, setNames] = useState(props.names);
 
+    const [inputNameValue, setInputNameValue] = useState("");
+
+    const getNewId = () => {
+        return Math.max(...names.map((name) => name.id)) + 1;
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setNames((prevNames) => [
+            ...prevNames,
+            { id: getNewId(), name: inputNameValue },
+        ]);
+
+        setInputNameValue("");
+    }
+
+    const handleInputNameChange = (event) => {
+        setInputNameValue(event.target.value);
+    }
+
+    const handleDelete = (id) => {
+        const newNames = names.filter((item) => item.id !== id);
+        setNames(newNames);
+    }
+
     return (
+        <>
         <ul>
             {names.map((item) => (
                 <li key={item.id}>
                     {item.name}
+                    <button value={item.id} onClick={() => handleDelete(item.id)}>Usuń</button>
                 </li>
             ))}
         </ul>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={inputNameValue}
+                onChange={handleInputNameChange}
+            />
+            <input type="submit" value="Dodaj"/>
+        </form>
+        </>
     );
 }
